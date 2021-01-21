3becea062262212e02734ee6311cd9dda9eb2974
Keiji Miyake <k.miyake01+dev@gmail.com>
2021-01-22 00:33:43 +0900

    update

--- WSL2/init.sh
+++ WSL2/init.sh
@@ -5,3 +5,6 @@ sudo apt upgrade
 sudo apt-get install vim tmux build-essential curl file git
 curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh | sh
 brew update
+# Powerline-Go install
+sudo apt install golang-go
+go get -u github.com/justjanne/poweroline-g
